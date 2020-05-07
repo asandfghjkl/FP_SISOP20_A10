552560a10601f8e27d8f728f66efcbc648ec6c19
Adrian Danindra <60964359+Bluejake3@users.noreply.github.com>
2020-05-07 12:38:08 +0700

    Update touch.c

--- touch/touch.c
+++ touch/touch.c
@@ -1,12 +1,6 @@
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <unistd.h>
 #include <syslog.h>
-#include <string.h>
 #include <string.h>
 #include <dirent.h>
 
